f78af7536767efeaf2fdcbefd6d4f857cf1e793a
arthur-rc18 <arthurrodri015@gmail.com>
2022-03-24 11:31:17 -0300

    Adding more info

--- pointer.go
+++ pointer.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+type Vertex struct {
+	sum  int
+	sum2 int
+}
+
 var p *int //When used to reference itself, the value will be 'nil'
 
 func point() {
@@ -12,5 +17,12 @@ func point() {
 
 	*p = 21 //Set i through the pointer p
 	//This is known as "dereferecing" or "indirecting"
+
+	v := Vertex{} //Struct fields can be accessed through a struct pointer
+	p := &v       //p is setting to p, which is a variable of the struct Vertex
+	p.sum = 1e9   //Here p is setting v's fields
+	p.sum2 = 1748
+
+	fmt.Println(v) //v will print, respectively, 1e9 in sum field and 1748 in sum2 field
 
 }
