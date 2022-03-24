fe94eb22b0da1d6cd6fb3da0e9c291912a439c4f
arthur-rc18 <arthurrodri015@gmail.com>
2022-03-24 14:20:19 -0300

    Introducing the slice concept

--- array.go
+++ array.go
@@ -15,6 +15,9 @@ func main() {
 	i := [2]int{5, 9} // Example of an array in a function
 	fmt.Println(sum(i[0], i[1]))
 
+	var j []int = primes[2:4] // This is called "slice", a slice is formed by specifyin two indices, a low and a high bound, separated by a colon.
+	fmt.Println(j)            // This selects a half-open range which includes the first element, but excludes the last one.
+
 }
 
 func sum(x, y int) int {
