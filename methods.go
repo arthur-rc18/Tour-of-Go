894873b7e3078983932575f0a3ce65451998330e
arthur-rc18 <arthurrodri015@gmail.com>
2022-03-29 10:01:01 -0300

    Adding more examples

--- methods.go
+++ methods.go
@@ -17,4 +17,23 @@ func main() {
 	v := Vert{3, 4}
 
 	fmt.Println(v.Abs())
+	
+	
+	f := Myfloat(-math.Sqrt2)
+	fmt.Println(f.Abs2()-)
+
 }
+
+// You can declare a method on non-struct types, too.
+type Myfloat float64
+
+// You can only declare a method with a receiver whose type is defined in the same package as the method.
+func (f Myfloat) Abs2() float64{
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+		
+}	
+
+
