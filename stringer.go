48d0814fa19c80481762002bdd0503904b15928f
arthur-rc18 <arthurrodri015@gmail.com>
2022-03-31 14:56:41 -0300

    Commenting the code

--- stringer.go
+++ stringer.go
@@ -7,12 +7,17 @@ type Person struct {
 	Age  int
 }
 
+// Stringer is implemented by any value that has a String method, which defines de "native" format for thar value
+// The String method is used to print values passed as an operand to any format that accepts a string or to an unformatted printer such as Print.
 func (p Person) String() string {
-	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
+	return fmt.Sprintf("%v (%v years)\n", p.Name, p.Age) // Inside the function, you associate the fields and customize how to print.
 }
 
 func main() {
 	a := Person{"Arthur Dent", 42}
 	z := Person{"Zaphod Beeblebrox", 9001}
 	fmt.Println(a, z)
+	// When executed, as defined in the String() function, will show
+	// Arthur Dent (42 years)
+	// Zaphod Beeblebrox (9001 years)
 }
