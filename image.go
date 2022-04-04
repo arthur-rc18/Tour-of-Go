959d86f69ecf3b7d48ae8fac93e6e28e40953bc9
arthur-rc18 <arthurrodri015@gmail.com>
2022-04-04 10:56:17 -0300

    Adding the coments

--- image.go
+++ image.go
@@ -18,7 +18,8 @@ import (
 
 func main() {
 
-	m := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	fmt.Println(m.Bounds())
-	fmt.Println(m.At(0, 0).RGBA())
+	// The NewRGBA function returns a new RGBA image with the given bounds
+	m := image.NewRGBA(image.Rect(0, 0, 100, 100)) // Rect is shorthand for rectangle{Pt(x0,y0),Pt(x1,y1)}
+	fmt.Println(m.Bounds())                        // The function Bounds
+	fmt.Println(m.At(0, 0).RGBA())                 // The At function explained previously
 }
