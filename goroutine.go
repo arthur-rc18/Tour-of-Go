c7df66922dfa2a9eafede65203e790b88991f1f5
arthur-rc18 <arthurrodri015@gmail.com>
2022-04-05 11:18:54 -0300

    Adding the main

--- goroutine.go
+++ goroutine.go
@@ -6,15 +6,23 @@ import (
 )
 
 func numbers() {
-	for i := 0; i <= 4; i++ {
-		time.Sleep(400 * time.Millisecond)
-		fmt.Printf("%d", i)
+	for i := 1; i <= 5; i++ {
+		time.Sleep(400 * time.Millisecond) // If you don't specify the time, it will be instantly.
+		fmt.Printf("%d ", i)
 	}
 }
 
 func alphabet() {
 	for i := 'a'; i <= 'e'; i++ {
 		time.Sleep(430 * time.Millisecond)
-		fmt.Printf("%d", i)
+		fmt.Printf("%c ", i)
 	}
+}
+
+// A goroutine is a lightweight thread managed by the Go runtime.
+func main() {
+	go numbers()  // This is how you start a goroutine
+	go alphabet() // These two goroutine will be executed at the same time, but with a difference of 30 Milliseconds, as setted before
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println("Main terminated")
 }
