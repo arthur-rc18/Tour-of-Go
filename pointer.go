c8a7ab526c27edc52467f24cf3ca40ee112d7a87
arthur-rc18 <arthurrodri015@gmail.com>
2022-04-05 14:19:25 -0300

    Adding the main function

--- pointer.go
+++ pointer.go
@@ -9,13 +9,13 @@ type Vertex struct {
 
 var p *int //When used to reference itself, the value will be 'nil'
 
-func point() {
+func main() {
 	i := 42         //i has the value of 42
 	var x = &i      //The & operator generates a pointer to its operand
 	fmt.Println(*x) //Read i through the pointer x
 	fmt.Println(x)  //When printed alone, will show the memory address of i
 
-	*p = 21 //Set i through the pointer p
+	//*p = 21 //Set i through the pointer p
 	//This is known as "dereferecing" or "indirecting"
 
 	v := Vertex{} //Struct fields can be accessed through a struct pointer
