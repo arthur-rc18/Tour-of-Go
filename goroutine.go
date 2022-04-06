79c343ce88a1c2cf6513f18832436b719e51f6bb
arthur-rc18 <arthurrodri015@gmail.com>
2022-04-06 14:39:44 -0300

    Fixing an error

--- goroutine.go
+++ goroutine.go
@@ -21,8 +21,8 @@ func alphabet() {
 
 // A goroutine is a lightweight thread managed by the Go runtime.
 func main() {
-	go numbers()  // This is how you start a goroutine
-	go alphabet() // These two goroutine will be executed at the same time, but with a difference of 30 Milliseconds, as setted before.
-	time.Sleep(3000 * time.Millisecond)
-	fmt.Println("Main terminated") // The goroutine will be terminated after the main goroutine, doesn't matter if has or not completed the execution.
+	go numbers()                        // This is how you start a goroutine
+	go alphabet()                       // These two goroutine will be executed at the same time, but with a difference of 30 Milliseconds, as setted before.
+	time.Sleep(3000 * time.Millisecond) // This means that the last command will take 3 seconds to be executed.
+	fmt.Println("Main terminated")      // The goroutine will be terminated after the main goroutine, even if has or not completed the execution.
 }
